607fe7d6fb99d3d4a9bc49a17f27c64343bcb07d
Philipp Janda <siffiejoe@gmx.net>
2014-04-11 06:48:01 +0200

    warped directory layout to existing luarocks windows layout

--- templates/luarocks-admin51.bat
+++ templates/luarocks-admin51.bat
@@ -2,7 +2,7 @@ echo off & setlocal
 
 set LR4WIN=@@DIR@@
 if not defined LUA_PATH set LUA_PATH=;
-set LUA_PATH=%LR4WIN%\luarocks\share\lua\luarocks\?.lua;%LR4WIN%\luarocks\share\lua\luarocks\5.1\?.lua;%LUA_PATH%
+set LUA_PATH=%LR4WIN%\luarocks\5.1\2.1\lua\?.lua;%LUA_PATH%
 %LR4WIN%\lua\lua51.exe %LR4WIN%\luarocks\bin\luarocks-admin.lua %*
 exit /B %errorlevel%
 
